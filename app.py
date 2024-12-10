514805db3cd57b26a1b5a5227cf7070ec48995be
francescazfl <francesca.zf.l@berkeley.edu>
2024-12-10 05:03:05 -0800

    minor color wording update

--- app.py
+++ app.py
@@ -277,7 +277,7 @@ def plot_bar_point(
 
         # Define bar line styles conditionally
         bar_lines = [
-            {"color": "black", "width": 2}
+            {"color": "#505050", "width": 2}
             if val == highlight_label
             else {"color": "white", "width": 0}
             for val in bar_data[x]
@@ -512,7 +512,7 @@ def plot_embxy(df: pd.DataFrame, product: str):
         y="y_coordinate",
         color=product,
         hover_data=["ID", "amino-acid_substitutions", "Parent_Name"],
-        title=f"{product} in Sequence Embedding Space",
+        title=f"{product} in sequence embedding space",
         color_continuous_scale="RdBu_r",
         labels={
             "x_coordinate": "Embedding x",
@@ -621,6 +621,7 @@ def seqfit_runner():
 
     # Generate embedding plot
     st.header("Sequence Embedding")
+    st.subheader("esm2_t12_35M_UR50D PCA")
     agg_embxy(append_xy(df, products=fold_products), products=fold_products)
 
     st.subheader("Done LevSeq!")
